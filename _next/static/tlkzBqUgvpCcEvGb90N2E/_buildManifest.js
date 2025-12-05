2f0593f4c95d5c76b69854f8a5a5b9c46504db82
eszter-with-a-z <eszter.with.a.z@gmail.com>
2025-12-05 22:32:30 +0100

    Updates

--- _next/static/tlkzBqUgvpCcEvGb90N2E/_buildManifest.js
+++ _next/static/tlkzBqUgvpCcEvGb90N2E/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/nora-julianna//_next/:path+",
+        "destination": "/nora-julianna/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
