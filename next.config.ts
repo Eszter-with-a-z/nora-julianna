a0af662832086dc72e1f87d7831e4d32f98475bd
eszter-with-a-z <eszter.with.a.z@gmail.com>
2025-12-06 01:31:11 +0100

    Hopefully add /out

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ const nextConfig = {
   output: "export",              // required for GitHub Pages
   basePath: "/nora-julianna",    // always use repo name
   assetPrefix: "/nora-julianna/",
-  reactStrictMode: true,
 
   images: {
     unoptimized: true,           // fixes static export issues with images
