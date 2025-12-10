0a3b787940e318d424299f652798ccdb36044db5
eszter-with-a-z <eszter.with.a.z@gmail.com>
2025-12-10 21:39:37 +0100

    Initial carousel

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,12 +1,23 @@
+"use client"
 import Link from "next/link"
 import Image from "next/image"
 import type { Project } from "@/lib/projects"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel"
+import Autoplay from "embla-carousel-autoplay"
+import { useRef } from "react"
 
 interface ProjectCardProps {
   project: Project
 }
 
 export default function ProjectCard({ project }: ProjectCardProps) {
+    const autoplay = useRef(
+    Autoplay({ delay: 2500, stopOnInteraction: false })
+  )
   return (
     <Link href={`/projects/${project.slug}`}>
       <div className="group cursor-pointer mb-16">
@@ -25,13 +36,24 @@ export default function ProjectCard({ project }: ProjectCardProps) {
 
           {/* Right column - image and keywords */}
           <div className="space-y-4">
-            <div className="relative w-full aspect-video bg-gray-100 overflow-hidden">
-              <Image
-                src={project.coverImage || "/placeholder.svg"}
-                alt={project.title}
-                fill
-                className="object-cover group-hover:opacity-90 transition-opacity"
-              />
+            <div className="">
+              <Carousel className="w-full max-w-sm mx-auto relative w-full aspect-video bg-gray-100 overflow-hidden"
+                    plugins={[autoplay.current]}
+                  
+                  opts={{ loop: true }}>
+                <CarouselContent>
+                  {project.images.map((image) => (
+                    <CarouselItem key={image} className="p-4">
+                      <Image
+                        src={image || "/placeholder.svg"}
+                        alt={""}
+                        fill
+                        className="object-cover group-hover:opacity-90 transition-opacity"
+                      />
+                    </CarouselItem>
+                  ))}
+                </CarouselContent>
+              </Carousel>
             </div>
             <div className="flex flex-wrap gap-2">
               {project.keywords.map((keyword) => (
